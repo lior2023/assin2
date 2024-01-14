4b49b9184f97662f83d916e116539552d1681a7e
Lior Aviely <154082705+lior2023@users.noreply.github.com>
2024-01-14 19:00:45 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,16 @@ export default function Home() {
     <body>
   
       <h1 >
-        LIOR'S CAKES
+        LIORS CAKES
       </h1>
       <iframe width="295" height="520" 
       src="https://www.youtube.com/embed/XbD612uBN8k?autoplay=1&mute=1&loop=1">
       </iframe>
 
       <h2 className='explain'>
-        welcome!<br></br>i'm here to make your dream come true!<br></br>
-        in the link below you can visit my instegram profile, <br></br>and you can press the button to make a resevation! 
-        <br></br> I'm here for any question!
+        welcome!<br></br>im here to make your dream come true!<br></br>
+        in the link below you can visit my instegram profile <br></br>and you can press the button to make a resevation! 
+        <br></br> Im here for any question!
       </h2>
       <button type = "button" onClick={Al}
       >

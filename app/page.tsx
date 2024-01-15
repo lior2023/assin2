94654d140c539d0c7a40ceebf2cc4680bc203344
Lior Aviely <154082705+lior2023@users.noreply.github.com>
2024-01-15 19:09:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,14 @@ export default function Home() {
   const Al = () => {
     alert("YAYYY THANK YOU!!")
   };
+  
+  const [active, setActive] = useState(false);
+
+  const changeColor = () => {
+    setActive(!active);
+  };
+  
+
   return (
     <body>
   
@@ -22,19 +30,30 @@ export default function Home() {
       <iframe width="295" height="520" 
       src="https://www.youtube.com/embed/XbD612uBN8k?autoplay=1&mute=1&loop=1">
       </iframe>
-
-      <h2 className='explain'>
-        welcome!<br></br>im here to make your dream come true!<br></br>
-        in the link below you can visit my instegram profile <br></br>and you can press the button to make a resevation! 
+      
+      <p className='explain'>
+        <strong>welcome!</strong><br></br>im here to make your dream come true!<br></br>
+        in the link below you can visit my <em>instegram</em> profile <br></br>and you can <em>press the button</em> to make a resevation! 
         <br></br> Im here for any question!
-      </h2>
-      <button type = "button" onClick={Al}
+        <ul>
+          <li>cakes</li>
+          <li>cookies</li>
+          <li>desserts</li>
+        </ul>
+      </p>
+      
+
+
+      <div className = "but2"  onClick={changeColor} style={{ backgroundColor: active ? "black" : "white" }}
       >
-        I want to make a reservation
-      </button>
-      <a href="https://www.instagram.com/lior.aviely?igsh=MW9wc3BpYnk1czlweA%3D%3D&utm_source=qr">to my instegram page</a> 
-    
-    
+        <a href="https://www.instagram.com/lior.aviely?igsh=MW9wc3BpYnk1czlweA%3D%3D&utm_source=qr">to my instegram page</a> 
+        <button type = "button" onClick={Al}
+        >
+          I want to make a reservation
+        </button>
+      </div>
+      <img src="https://media.tenor.com/EEemcSEaAcAAAAAj/its-really-tasty-candice-hutchings.gif" alt="tasty face"  width="100" height="100"></img>
+
     </body>
   )
 }
